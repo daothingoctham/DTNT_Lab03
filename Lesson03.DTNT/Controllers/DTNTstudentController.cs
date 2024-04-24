cf82cdfaad2c081737f6382dcd88fd2477c5bf73
daothingoctham <ngoctham030104@gmail.com>
2024-04-24 17:53:57 +0700

    add file html.css.js

---
+++ Lesson03.DTNT/Controllers/DTNTstudentController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lesson03.DTNT.Controllers
+{
+    public class DTNTstudentController : Controller
+    {
+        // GET: DTNTstudent
+        public ActionResult Index()
+        {
+            ViewData["msg"] = "View Data- ĐÀO NGỌC THẮM";
+            ViewData["time"] = DateTime.Now.ToString("hh:mm:ss dd/MM/yyyy tt");
+            return View();
+        }
+        public ActionResult StudentList()
+        {
+            ViewBag.titleName = "Danh sách học viên - Đào Ngọc Thắm";
+            string[] names = { "Tạ Hồng", "Ngọc Thắm", "Diễm Hương", "Kim Oanh" };
+            ViewBag.list = names;
+            var obj = new
+            {
+                ID = 100,
+                Name = "Ngọc Thắm",
+                Age = 20
+            };
+            ViewBag.student = obj;
+            return View();
+        }
+        public ActionResult Insert()
+        {
+            return View("AddStudent");
+        }
+    }
+}
